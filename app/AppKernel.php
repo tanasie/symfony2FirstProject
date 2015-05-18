cf8aa4dbc9eb58767750707df4200345f23b5054
tanasie <capalna.tanase@gmail.com>
2015-05-18 14:59:14 +0300

    Refactoring code.
    Deleted AppBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,7 +22,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new FOS\RestBundle\FOSRestBundle(),
